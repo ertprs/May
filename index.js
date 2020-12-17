abf01cbc9d42d6df44c7220c49e86e7318d93dec
thothdeus <76016725+thothdeus@users.noreply.github.com>
2020-12-17 19:26:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -599,14 +599,14 @@ axios.get(`https://alfians-api.herokuapp.com/api/yta?url=${teks}`).then((res) =>
 
 if (text.includes("Thoth")){
 const aris = text.replace(/Thoth /, "")
-axios.get(`https://arugaz.herokuapp.com/api/simisimien?text=}`).then((res) => {
+axios.get(`https://arugaz.herokuapp.com/api/simisimi?kata=}`).then((res) => {
     let hasil = `${res.data.result}`;
     conn.sendMessage(id, hasil ,MessageType.text);
   })
  }
  if (text.includes("Bot")){
 const aris = text.replace(/Bot /, "")
-axios.get(`https://arugaz.herokuapp.com/api/simisimien?text=}`).then((res) => {
+axios.get(`https://arugaz.herokuapp.com/api/simisimi?kata=}`).then((res) => {
     let hasil = `${res.data.result}`;
     conn.sendMessage(id, hasil ,MessageType.text);
   })
