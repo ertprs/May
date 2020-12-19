926c9bebfecb0b25f38dd3734d60ddf217efbb6a
thothdeus <76016725+thothdeus@users.noreply.github.com>
2020-12-18 22:03:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -779,26 +779,10 @@ if (text.includes("#start"))
     }
 	
 if (text.includes("#porn")){
-const teks = text.replace(/!porn/, "")
+const teks = text.replace(/#porn/, "")
 axios.get(`https://mnazria.herokuapp.com/api/porn?search=${teks}`).then((res) => {
     let porno = ` *LISTA DOS RESULTADOS* \n\n *Canal:* ${res.data.result[0].actors} \n\n *DURAÇÃO:* ${res.data.result[0].duration}  \n\n *TITULO:* ${res.data.result[0].title}\n\n *URL:* ${res.data.result[0].url}`;
     conn.sendMessage(id, porno ,MessageType.text);
-})
-}
-	
-if (text.includes("#checkip")){
-const teks = text.replace(/#checkip/, "")
-axios.get(`https://mnazria.herokuapp.com/api/check?ip=${teks}`).then((res) => {
-    let hasil = `${result}`;
-    conn.sendMessage(id, hasil ,MessageType.text);
-})
-}
-	
-if (text.includes("#xvideo")){
-const teks = text.replace(/#xvideo /, "")
-axios.get(`https://tobz-api.herokuapp.com/api/xvideos?q=${teks}`).then((res) => {
-    let hasil = `DOSA BORR\n\n${result}`;
-    conn.sendMessage(id, hasil ,MessageType.text);
 })
 }
 	
